4d3514c0099585ccfdfb7e0b95f56487ee474a53
zce <w@zce.me>
2018-01-28 18:00:40 +0800

    :art: 微调代码结构 & 注释

--- models/index.js
+++ models/index.js
@@ -1,30 +1,29 @@
-const path = require('path')
+/**
+ * 自动载入所有模型并导出
+ * sequelize 的使用可以参考：
+ * - https://github.com/sequelize/express-example
+ */
 
-// 自动匹配路径
 const glob = require('glob')
-// ORM 框架
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize({
-  dialect: 'mysql',
-  host: '127.0.0.1',
-  database: 'newshop2',
-  username: 'root',
-  password: 'wanglei',
-  logging: false,
-  define: {
-    timestamps: false
-  }
-})
+const config = require('../config')
 
+const sequelize = new Sequelize(Object.assign(config.database, {
+  // 由于数据库每张表中没有 sequelize 对应的时间戳字段，
+  // 所以必须关闭 sequelize 自动生成时间戳的功能
+  define: { timestamps: false }
+}))
+
+// 自动载入当前目录下所有的 JS 文件
 glob.sync('*.js', { cwd: __dirname })
+  // 忽略 index.js 文件，因为它不是模型文件
   .filter(item => item !== 'index.js')
-  .forEach(item  => {
-    const model = sequelize.import(path.join(__dirname, item))
+  // 遍历每一个模型文件名
+  .forEach(item => {
+    // 通过 sequelize 导入模型
+    // 注意：这里的模型文件不能单独使用，必须通过 `sequelize.import()` 才可以使用
+    const model = sequelize.import(item)
+    // 将模型对象导出
     exports[model.name] = model
   })
-
-// sequelize.import(path.join(__dirname, 'attribute.js'))
-// sequelize.import(path.join(__dirname, 'attribute.js'))
-// sequelize.import(path.join(__dirname, 'attribute.js'))
-// sequelize.import(path.join(__dirname, 'attribute.js'))
